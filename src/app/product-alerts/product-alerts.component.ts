3467d9a298a845a15b18234c90d5cabe831ece21
randomnon <randomnon868@gmail.com>
2021-01-16 20:42:00 -0800

    1 file modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from "@angular/core";
-import { Input } from "@angular/core";
-import { Output, EventEmitter } from "@angular/core";
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
 
 @Component({
-  selector: "app-product-alerts",
-  templateUrl: "./product-alerts.component.html",
-  styleUrls: ["./product-alerts.component.css"]
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
 })
-
-//The @Component() definition also exports the class, //ProductAlertsComponent, which handles functionality for the //component.
 export class ProductAlertsComponent {
   @Input() product;
   @Output() notify = new EventEmitter();
+}
+
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
